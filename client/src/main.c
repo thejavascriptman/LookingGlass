b72b7f0a3b38c91c2611bd6e4cba10b680b0b8b7
thejavascriptman <thejavascriptman@outlook.com>
2019-11-13 23:35:36 +0900

    respect minimizeOnFocusLoss

--- client/src/main.c
+++ client/src/main.c
@@ -1101,7 +1101,7 @@ int run()
     return 1;
   }
 
-  if (params.fullscreen || !params.minimizeOnFocusLoss)
+  if (params.fullscreen && !params.minimizeOnFocusLoss)
     SDL_SetHint(SDL_HINT_VIDEO_MINIMIZE_ON_FOCUS_LOSS, "0");
 
   if (!params.noScreensaver)
